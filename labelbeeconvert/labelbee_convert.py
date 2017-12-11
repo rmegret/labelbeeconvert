444157f0a85e1cde6dceb952b9d4d460a96b30e0
Remi Megret <remi.megret@upr.edu>
2017-12-11 14:08:01 -0400

    - labelbee_convert.py:   - new methods for loading tags and tracks directly to DataFrame   - new methods for loading datasets   - separate false alarm from wrongid   - bug fix: handle empty label string   - bug fix: better filename parsing for C02_YYMMDDhhmmss format - test_tracks.py: more code for plotting tags properties

--- labelbeeconvert/labelbee_convert.py
+++ labelbeeconvert/labelbee_convert.py
@@ -17,7 +17,9 @@ def load_tags_json(filename):
 
 
 def tracks_to_df(T):
-    df = pd.DataFrame(columns=['frame','id','leaving','entering','pollen','walking','fanning','FA'])
+    df = pd.DataFrame(columns=['frame','id','leaving','entering',
+                               'pollen','walking','fanning',
+                               'falsealarm','wrongid'])
     df = df.astype("bool")
     df[['frame','id']] = df[['frame','id']].astype("int64")
     
@@ -28,22 +30,27 @@ def tracks_to_df(T):
         if (frameDict is None): continue
         for id in frameDict:
             item=frameDict[id]
-            if (item['labels'] == ''):
+            if ('labels' not in item):
+                labelsstr=''
+            else:
+                labelsstr=item['labels']
+            if (labelsstr == ''):
                 labels=[]
             else:
-                labels=item['labels'].split(',')
+                labels=labelsstr.split(',')
             for l in labels:
                 if (l not in all_labels): all_labels.append(l)
             if (id not in all_ids): all_ids.append(id)
             df=df.append(dict(frame=int(item['frame']),
                               id=int(item['ID']),
-                              labels=item['labels'],
+                              labels=labelsstr,
                               leaving='leaving' in labels,
                               entering='entering' in labels,
                               pollen='pollen' in labels,
                               walking='walking' in labels,
                               fanning='fanning' in labels,
-                              FA='falsealarm' in labels or 'wronglabel' in labels
+                              falsealarm='falsealarm' in labels,
+                              wrongid='wrongid' in labels
                               )
                         ,ignore_index=True)   
     #print(all_labels)
@@ -56,7 +63,11 @@ def tags_to_df(Tags):
     
     all_ids=[]
     
-    for framestr in Tags:
+    frames = sorted(Tags.keys(), key=lambda x: int(x))
+    LD = []
+    
+    #print('Loading to list of record...')
+    for framestr in frames:
         frame=int(framestr)
         frameRecord = Tags[framestr]
         if (frameRecord is None): continue
@@ -68,12 +79,29 @@ def tags_to_df(Tags):
             D['hamming'] = item.get('hamming')
             D['c'] = item.get('c')
             D['p'] = item.get('p')
-            df=df.append(D,ignore_index=True)   
+            D['dm'] = item.get('dm')
+            LD.append(D)
+
+    #df=df.append(D,ignore_index=True)   
+    #print('Converting to DataFrame...')
+    df=pd.DataFrame(LD,columns=['frame','id','hamming','c','p','dm'])   
     #print(all_labels)
     #print(all_ids)
     return df
 
+def load_tracks_df(filename):
+    with open(filename,"r") as f:
+        T=json.load(f)
+    return tracks_to_df(T)
+
+def load_tags_df(filename):
+    with open(filename,"r") as f:
+        T=json.load(f)
+    return tags_to_df(T)
+
 def timestamping(df,timestring,fps=20):
+    if (df.shape[0]==0): return df   # Abort if empty
+    
     t0=pd.Timestamp(timestring)
     df = df.reindex(columns = np.append( df.columns.values, ['time']))
     df[['time']] = df[['time']].astype(pd.Timestamp)
@@ -83,15 +111,24 @@ def timestamping(df,timestring,fps=20):
     
     return df
 
-def timestamp_from_filename(filename):
-    result = re.match(r'.*?C(\d\d)_((\d\d\d\d\d\d)(\d\d\d\d\d\d))', filename)
+def parse_filename(filename):
+    result = re.match(r'.*?(C(\d\d)_((\d\d\d\d\d\d)(\d\d\d\d\d\d)))', filename)
     if (result is None): return None # Default
-    videoname=result.group(0)
-    camera_id=result.group(1)
-    timestamp='20'+result.group(2)
-    date=result.group(3)
-    time=result.group(4)
-    return timestamp
+    videoname=result.group(1)
+    camera_id=result.group(2)
+    timestamp='20'+result.group(3)
+    date=result.group(4)
+    time=result.group(5)
+    return {'videoname': videoname, 
+            'camera_id': camera_id,
+            'timestamp': timestamp,
+            'date': date,
+            'time': time}
+
+def timestamp_from_filename(filename):
+    info = parse_filename(filename)
+    if (info is None): return None
+    return info['timestamp']
 
 def load_fileset(inputlist):
     '''
@@ -101,20 +138,71 @@ def load_fileset(inputlist):
                 header=0,names=['filename'])
     L[['timestamp']]=L[['filename']].applymap(timestamp_from_filename)
     
-    df=pd.DataFrame()
-    for index, row in L.iterrows():
-        filename=row['filename']
+    df_list=[]
+    for index, fileinfo in L.iterrows():
+        filename=fileinfo['filename']
         print("Loading {}...".format(filename))
-        T=load_tracks_json(filename)
-        df1=tracks_to_df(T)
-        df1=timestamping(df1, row['timestamp'])
-        df=df.append(df1,ignore_index=True)
+        
+        infos=parse_filename(filename)
+        
+        #T=load_tracks_json(filename)
+        #df1=tracks_to_df(T)
+        df1 = load_tracks_df(filename)
+        df1=timestamping(df1, fileinfo['timestamp'])
+        if (infos is None):
+            df1['video']=''
+        else:
+            df1['video']=infos['videoname']
+        df_list.append(df1)
+        
+    print("Merging into single DataFrame...")
+    df = pd.concat(df_list)
 
     df[['datetime']]=df[['time']].applymap(lambda d: pd.to_datetime(d))
         
     #df = df.query('FA!=True')
     
     return df
+
+def load_tags_fileset(inputlist):
+    '''
+    ex: inputlist="/Users/megret/Documents/Research/BeeTracking/Soft/labelbee/python/tags_files.csv"
+    '''
+    L = pd.read_csv(inputlist,
+                header=0,names=['filename'])
+    L[['timestamp']]=L[['filename']].applymap(timestamp_from_filename)
+    
+    df_list=[]
+    for index, fileinfo in L.iterrows():
+        filename=fileinfo['filename']
+        print("Loading {}...".format(filename))
+        
+        infos=parse_filename(filename)
+        
+        #T=load_tracks_json(filename)
+        #df1=tracks_to_df(T)
+        df1 = load_tags_df(filename)
+        df1=timestamping(df1, fileinfo['timestamp'])
+        if (infos is None):
+            df1['video']=''
+        else:
+            df1['video']=infos['videoname']
+        if (df1.shape[0]>0): # Do not add empty DataFrames
+            df_list.append(df1)
+        else:
+            print('Warning: empty data from "{}"'.format(filename))
+        
+    print("Merging into single DataFrame...")
+    df = pd.concat(df_list)
+    
+    #df.index = range(df.shape[0])
+
+    df[['datetime']]=df[['time']].applymap(lambda d: pd.to_datetime(d))
+        
+    #df = df.query('FA!=True')
+    
+    return df
+
 
 def plot_activities(df):
     
@@ -126,11 +214,13 @@ def plot_activities(df):
     rmap = {id: i for i,id in enumerate(ids)}    
     df[['uid']]=df[['id']].applymap(lambda x: rmap[x])
     
+    df['FA']=df['falsealarm'] | df['wrongid']
+    
     fig=plt.figure()
-    idx=df.query('not FA and not walking and not fanning and not pollen and not entering and not leaving').index
+    idx=df.query('not falsealarm and not wrongid and not walking and not fanning and not pollen and not entering and not leaving').index
     plt.plot(df['datetime'][idx].tolist(),df['uid'][idx],'.',c='k',label='other')
-    idx=df.index[df['FA'].astype(bool)]
-    plt.plot(df['datetime'][idx].tolist(),df['uid'][idx],'x',c='#a0a0a0',label='FA/WId',mfc='none')
+    idx=df.query('falsealarm or wrongid').index
+    plt.plot(df['datetime'][idx].tolist(),df['uid'][idx],'x',c='#a0a0a0',label='FA',mfc='none')
     idx=df.index[df['walking'].astype(bool)]
     plt.plot(df['datetime'][idx].tolist(),df['uid'][idx],'o',c='k',label='walking',mfc='none')
     idx=df.index[df['fanning'].astype(bool)]
--- test/test_tracks.py
+++ test/test_tracks.py
@@ -6,27 +6,155 @@ Created on Fri Nov 24 19:28:38 2017
 @author: megret
 """
 
+
 #%%
-#%load_ext autoreload
-#%autoreload 2
+%load_ext autoreload
+%autoreload 2
 
 # In[]:
 import sys
-sys.path.append('/Users/megret/Documents/Research/BeeTracking/Soft/labelbee/python')
+sys.path.append('/Users/megret/Documents/Research/BeeTracking/Soft/labelbeeconvert')
 
+#%%
+from labelbeeconvert import labelbee_convert as lb
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # In[]:
 cd '/Users/megret/Documents/Research/BeeTracking/Soft/labelbeeconvert/data'
 
 #%%
-from labelbeeconvert import labelbee_convert as lb
-import pandas as pd
+
+evts=lb.load_fileset('inputlist_Gurabo.csv')
+tags=lb.load_tags_fileset('tags_files.csv')
+#df = df.query('FA!=True')
+#lb.plot_activities(df)
+
+#%%
+def compute_tagsize(p):
+    return np.sqrt(((p[0][0]-p[2][0])**2+(p[0][1]-p[2][1])**2
+                    + (p[1][0]-p[3][0])**2+(p[1][1]-p[3][1])**2)/2)
+
+tags[['tagsize']]=tags[['p']].applymap(lambda p: compute_tagsize(p))
+
+#%%
+def compute_tagminedge(p):
+    return np.sqrt(
+            np.min([
+                    (p[0][0]-p[1][0])**2+(p[0][1]-p[1][1])**2,
+                    (p[1][0]-p[2][0])**2+(p[1][1]-p[2][1])**2,
+                    (p[2][0]-p[3][0])**2+(p[2][1]-p[3][1])**2,
+                    (p[3][0]-p[0][0])**2+(p[3][1]-p[0][1])**2
+                    ])
+                    )
+
+tags[['tagminedge']]=tags[['p']].applymap(lambda p: compute_tagminedge(p))
+
+#%%
+def compute_area(p):
+    return np.abs( (p[1][0]-p[0][0])*(p[3][1]-p[0][1])
+             - (p[1][1]-p[0][1])*(p[3][0]-p[0][0])
+            ) + np.abs(
+                (p[1][0]-p[2][0])*(p[3][1]-p[2][1])
+                - (p[1][1]-p[2][1])*(p[3][0]-p[2][0])
+                    )
+,
+
+tags[['area']]=tags[['p']].applymap(lambda p: compute_area(p))
 
 #%%
 
-df=lb.load_fileset('inputlist.csv')
-df = df.query('FA!=True')
-lb.plot_activities(df)
+def compute_x(c):
+    return c[0]
+def compute_y(c):
+    return c[1]
+
+tags[['x']]=tags[['c']].applymap(lambda c: compute_x(c))
+tags[['y']]=tags[['c']].applymap(lambda c: compute_y(c))
 
 #%%
 print(df['labels'].unique())
+
+
+#%%
+
+
+
+# In[]:
+cd '/Users/megret/Documents/Research/BeeTracking/Soft/labelbee/data/Gurabo'
+
+# In[]:
+tags = lb.load_tags_df('Tags-C02_170624090000-raw.json')
+evts = lb.load_tracks_df('workshop/C02_170624090000-Tracks-Grace.json')
+
+# In[]:
+plt.plot(evts['frame'],evts['id'],'o')
+plt.plot(tags['frame'],tags['id'],'.')
+
+# In[]:
+e2=evts.copy()
+e2=e2.merge(tags,'left', on=['video','frame','id'])
+e3=e2.loc[e2['dm'].notnull(),['video','frame','id','dm','falsealarm','wrongid','c','p']]
+#e3=e2.loc[e2['dm'].notnull(),:]
+
+#%%
+m=0
+
+L=[]
+for i in range(-m,m+1):
+    tmp = evts.copy()
+    tmp[['frame']] = tmp[['frame']]+i
+    L.append(tmp)
+
+E = pd.concat(L)
+E = E.merge(tags,'left', on=['video','frame','id'])
+E = E.loc[E['dm'].notnull(),:]
+
+#E = E.query('dm>20 and tagsize>35')
+E = E.query('dm>20')
+
+#%%
+plt.figure()
+
+#prop = 'dm'
+prop = 'tagminedge'
+
+H,edges=np.histogram(E[prop],100)
+plt.plot(edges[:-1],H,label='all')
+
+H_FA,edges2=np.histogram(E.query('falsealarm')[prop],edges)
+plt.plot(edges[:-1],H_FA,label='falsealarm')
+
+H_WI,edges2=np.histogram(E.query('wrongid')[prop],edges)
+plt.plot(edges[:-1],H_WI,label='wrongid')
+
+H2,edges2=np.histogram(E.query('not wrongid and not falsealarm')[prop],edges)
+plt.plot(edges[:-1],H2,label='correct')
+
+plt.legend()
+
+#%%
+plt.figure()
+H,edges=np.histogram(df.loc[df['dm'].notnull(),'dm'],100,)
+plt.plot(edges[:-1],H,label='all')
+
+#%%
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import colors
+fig=plt.figure()
+
+cls=E['wrongid']+2*(E['falsealarm'] & ~E['wrongid'])
+
+classes=['OK','WId','FA']
+cmap = colors.ListedColormap(['green', 'blue','red'])
+centers=range(cmap.N)
+bounds=np.array(range(cmap.N+1))-0.5
+norm = colors.BoundaryNorm(bounds, cmap.N)
+
+ax = fig.add_subplot(111, projection='3d')
+handle=ax.scatter(E['tagsize'],E['area'],E['tagminedge'],s=1,
+                  c=cls, cmap=cmap, norm=norm)
+cbar=plt.colorbar(mappable=handle)
+cbar.set_ticks(centers)
+cbar.set_ticklabels(classes)
